4074c63dc56bb4e8ffb5541e35a6c6ef806faf53
bellkevin677 <bellkevin677@gmail.com>
2021-02-23 10:43:57 -0600

    complete

---
+++ variableExercise.js
@@ -0,0 +1,14 @@
+// Const
+let num = 20;
+num -= 5;
+num += 7;
+num--;
+num = "This is not a number"
+let blank;
+blank = null;
+console.log(blank);
+console.log(num);
+let quarter = .25, dime = .10, nickel = .05, penny = .01;
+console.log(quarter, dime, nickel, penny);
+let letters = "qwertyuiop";
+console.log(letters[9]);
